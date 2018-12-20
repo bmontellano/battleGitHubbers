2aab69101db9c61697ea9d62270df268f4ff585d
bmontellano <bmontellano@gmail.com>
2018-12-19 18:05:55 -0800

    added webpack configuration, but div is not populating, why????

--- app/components/App.js
+++ app/components/App.js
@@ -4,7 +4,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World!
+                Hello World!!!!!
+                <p>hello1111</p>
             </div>
         )
     }
