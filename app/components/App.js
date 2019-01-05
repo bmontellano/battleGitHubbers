a8cae80f35ad1fb37939ef0ab0ec123ea9269ac7
bmontellano <bmontellano@gmail.com>
2019-01-05 14:22:42 -0800

    updated body font family, created Populist class or module

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,13 @@
 var React = require('react')
+var Populist = require('./Populist')
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className = 'container'>
                 Hello World!!!!!
                 <p>hello1111</p>
+                <Populist />
             </div>
         )
     }
---
+++ app/components/Populist.js
@@ -0,0 +1,14 @@
+var React = require('react')
+
+class Populist extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3 id='populist-intro'>I'm a POPULIST</h3>
+            </div>
+        )
+    }
+    
+}
+
+module.exports = Populist;
